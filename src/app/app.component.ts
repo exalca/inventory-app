6fbe5c7ae5111f4c2e2eac985463fd9b93291b7b
Danish Ahmad <80050@EXALCA.CORP>
2018-09-18 12:04:01 +0530

    adding product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Product } from './product/product.model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // selector: 'app-root',
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
+  // tslint:disable-next-line:component-selector
+  selector: 'inventory-app-root',
+  // template: `<div class="inventory-app">
+  //               (Products will go here soon)
+  //           </div>`,
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'inventory-app';
+  product: Product;
+
+  constructor() {
+    // tslint:disable-next-line:prefer-const
+    let newProduct = new Product(
+      'NICEHAT',
+      'A nice black hat',
+      '/assets/images/products/black-hat.jpg',
+      ['Men', 'Accessories', 'Hats'],
+      29.99
+    );
+
+    this.product = newProduct;
+  }
 }
